01bfbf0824d7fbb54b9e78858aed1caa1c120b0f
petahcole <cbatson89@gmail.com>
2016-12-08 22:04:14 -0700

    completed basic backend

--- server/index.js
+++ server/index.js
@@ -13,3 +13,27 @@ app.listen(port, () =>   {
     console.log(`listening on ${port}`);
 });
 
+app.get("/books", (req, res, next) =>  {
+    knex("books").then((data) => {
+        res.json(data);
+    })
+})
+
+app.get("/books/:id", (req, res, next) =>   {
+    knex("books").where("id", req.params.id).first().then((data)=>   {
+        res.json(data);
+    })
+})
+
+app.post("/books", (req, res, next) => {
+    console.log(req.body);
+    knex("books").insert({
+        "title": req.body.title,
+        "author": req.body.author,
+        "genre": req.body.genre,
+        "read": req.body.read
+    },"*").then((date) =>  {
+        console.log("success!");
+        res.json(data)
+    })
+})
